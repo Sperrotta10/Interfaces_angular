de6832772b49aa99c009c4fe4c5e12ea1d4b68bd
Juan Wu <juanwu8238@gmail.com>
2025-06-22 12:14:33 -0400

    Preparando estructura para la plantilla, agregar componentes: header y footer

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  template: `
+    <app-header />
+    <main>
+      <router-outlet />
+    </main>
+    <app-footer />
+    
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
