23f268f7c42f6cf2ef966b78afb137253a381ad8
Juan Wu <juanwu8238@gmail.com>
2025-06-22 16:51:12 -0400

    Vista de cambio de color y cambia tamaño de letra

--- angular/src/app/view/admin/admin.component.ts
+++ angular/src/app/view/admin/admin.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+//Valores de colores defectos
+  headerBgColor = '#3498db';
+  titleColor = '#ffffff';
+  divBorderColor = '#95a5a6';
+  footerBgColor = '#2c3e50';
+  cardBgColor = '#ecf0f1';
 
+  titleFontSize = 36;
+  
 }
