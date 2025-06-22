3dc0ff8d3e0ec2634f0d59aad95a395d1259f7b0
Juan Wu <juanwu8238@gmail.com>
2025-06-22 12:32:14 -0400

    Agregar rutas y vista de admin(componente)

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+        return import('./view/home/home.component').then(
+            m => m.HomeComponent //la palabra m se refiere module
+        )
+    },
+},
+{
+    path:'admin',
+    loadComponent: () => {
+        return import ('./view/admin/admin.component').then(
+            m => m.AdminComponent
+        )
+    }
+}
+];
