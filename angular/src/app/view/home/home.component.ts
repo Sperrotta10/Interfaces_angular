5a81bc59d1807bb6d4edb13fc08a532f14dba252
Juan Wu <juanwu8238@gmail.com>
2025-06-22 12:38:57 -0400

    agrega componentes: about-section y services-section

--- angular/src/app/view/home/home.component.ts
+++ angular/src/app/view/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
+import { AboutSectionComponent } from '../../components/about-section/about-section.component';
+import { ServicesSectionComponent } from '../../components/services-section/services-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, AboutSectionComponent, ServicesSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
