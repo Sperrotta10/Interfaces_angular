5fd553ab2a4b6e19011b42526f1bbe1d84385f22
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-06-23 15:20:54 -0400

    feat (a..router and app.config): Se importo los modulos necesarios para trabajar con fetch (api) y se creo un componente con la ruta (test-api) para comprobar el funcionamiento

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,21 +1,29 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { TestApiComponent } from './components/test-api/test-api.component';
+import { Component } from '@angular/core';
 
 export const routes: Routes = [
     {
-    path: '',
-    pathMatch: 'full',
-    loadComponent: () => {
-        return import('./view/home/home.component').then(
-            m => m.HomeComponent //la palabra m se refiere module
-        )
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./view/home/home.component').then(
+                m => m.HomeComponent //la palabra m se refiere module
+            )
+        },
     },
-},
-{
-    path:'admin',
-    loadComponent: () => {
-        return import ('./view/admin/admin.component').then(
-            m => m.AdminComponent
-        )
+    {
+        path:'admin',
+        loadComponent: () => {
+            return import ('./view/admin/admin.component').then(
+                m => m.AdminComponent
+            )
+        }
+    },
+    {
+        path: 'test-api',
+        component: TestApiComponent
     }
-}
+
 ];
