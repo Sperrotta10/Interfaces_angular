fa0575dcb6b66e54699504ca1464a81761377709
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-06-29 16:43:15 -0400

    feat (view notfound): Se implemento una vista generica para los usuarios que accedan a rutas que no existen en nuestra pagina

---
+++ angular/src/app/view/not-found/not-found.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-in', style({ opacity: 1 }))
+      ])
+    ])
+  ]
+})
+export class NotFoundComponent {
+
+}
